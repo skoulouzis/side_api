802a8815e069f1f82598c5398a507d39a2263ba3
David Rogers <dmckrogers@gmail.com>
2017-01-31 21:05:51 +0000

    ignoring keys

--- side_api/settings.py
+++ side_api/settings.py
@@ -12,15 +12,13 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from settings_secret import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+*)qjf=wgq26il#9fm2ji##y^l8==oal0&7_s7u^d!y@g0t4=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ side_api/settings_secret.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '+*)qjf=wgq26il#9fm2ji##y^l8==oal0&7_s7u^d!y@g0t4=^'
