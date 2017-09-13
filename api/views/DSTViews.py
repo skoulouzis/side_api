529a3ff36b7832f84ac2a6113a2006f6000ed949
Matej Cigale <cigalem@cardiff.ac.uk>
2017-09-13 13:03:15 +0100

    Changes to DST

--- api/views/DSTViews.py
+++ api/views/DSTViews.py
@@ -1,17 +1,54 @@
-from rest_framework import viewsets
+import requests
+
+from requests.auth import HTTPBasicAuth
+
+from django.http import JsonResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.db.models import Q
+from rest_framework import viewsets, status
+from rest_framework.decorators import list_route, detail_route
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_extensions.mixins import PaginateByMaxMixin
 
+from api.models import DSTInstance
+from side_api import settings, utils
+from api.services import JenaFusekiService, DripManagerService
+import json
 
-from api.models import *
-from api.serializers import *
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 
-class DSTInstanceRequest(APIView):
+class DSTInstanceRequest(PaginateByMaxMixin, viewsets.ModelViewSet):
 
-    def post(self, request):
-        dst_service_id = request.POST.get('dst_service_id')
-        return Response("ffaf")
+    @detail_route(methods=['post'], permission_classes=[])
+    def DSTinstance(self, request, pk=None):
+
+        request_json = json.loads(request.body)
+        service_id = request_json["serviceId"]
+
+        instance = DSTInstance.create(service_id)
+        instance.save()
+        json_result = {
+            'instance_id': instance.id
+        }
+        return JsonResponse(json_result)
+
+    @detail_route(methods=['post'], permission_classes=[])
+    def DSTrequest(self, request, pk=None):
+
+
+        json_result = {
+            'url': 'some.url',
+            'text': 'status'
+        }
+
+        return JsonResponse(json_result)
+
+    @detail_route(methods=['get'], permission_classes=[])
+    def DSTupdate(self, request, pk=None):
+
+        json_result = {
+            'update': 'status'
+        }
+
+        return JsonResponse(json_result)
